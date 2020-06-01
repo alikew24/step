5bf5cf1a721cbd6137e998f0a11e25ee4efa4b7d
Alisha Kewalramani <alikew24@gmail.com>
2020-06-01 14:01:20 -0400

    Remove local variable and substitue for static final variable

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -28,8 +28,7 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String message = WELCOME_MESSAGE;
     response.setContentType("text/html;");
-    response.getWriter().println(message);
+    response.getWriter().println(WELCOME_MESSAGE);
   }
 }
